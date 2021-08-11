4bb251bac54657f1e65de32eac070af0127f7c4b
Mikhail Gavrilov <mjgavrilov@gmail.com>
2021-08-11 20:21:19 +0300

    Create main.go

---
+++ main/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+  "os"
+  "time"
+  "log"
+  "strconv"
+  miniredis "github.com/alicebob/miniredis/v2"
+)
+
+func forever() {
+    for {
+        time.Sleep(time.Second)
+    }
+}
+
+func main() {
+  m := miniredis.NewMiniRedis()
+  port, _ := strconv.Atoi(os.Args[1])
+  m.port := port
+  err := m.Start()
+
+  if err != nil {
+    log.Printf("Error %s", err)
+    os.Exit(1)
+  }
+
+  defer m.Close()
+
+  go forever()
+  select {}
+}
